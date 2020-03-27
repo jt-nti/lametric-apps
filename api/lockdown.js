97ab8976bf39fde94c1ec32e808baa16eb7b587d
James Taylor <jt-git@nti.me.uk>
2020-03-27 18:34:18 +0000

    Initial display option support
    Signed-off-by: James Taylor <jt-git@nti.me.uk>

--- api/lockdown.js
+++ api/lockdown.js
@@ -7,19 +7,50 @@ module.exports = (req, res) => {
     const elapsed = length - (end - now);
     const progress = Math.round(elapsed / length * 100);
 
-    const lametricFrames = {
-        frames: [
-            {
-                goalData: {
-                    start: 0,
-                    current: progress,
-                    end: 100,
-                    unit: '%'
-                },
-                icon: 'a36057'
-            }
-        ]
+    const titleFrame = {
+        text: 'Lockdown',
+        icon: 'a36057',
+        index: null
+    };
+
+    const dayFrame = {
+        text: 'Wed',
+        icon: 'i1324',
+        index: null
+    };
+
+    const progressFrame = {
+        goalData: {
+            start: 0,
+            current: progress,
+            end: 100,
+            unit: '%'
+        },
+        icon: 'a36057',
+        index: null
+    };
+
+    const lametricResponse = {
+        frames: []
+    };
+
+    if (req.query.display) {
+        if (req.query.display.includes('Title')) {
+            lametricResponse.frames.push(titleFrame);
+        }
+        if (req.query.display.includes('Day')) {
+            lametricResponse.frames.push(dayFrame);
+        }
+        if (req.query.display.includes('Progress')) {
+            lametricResponse.frames.push(progressFrame);
+        }
+    } else {
+        lametricResponse.frames.push(progressFrame);
     }
 
-    res.status(200).json(lametricFrames);
+    lametricResponse.frames.forEach(function(frame, index){
+        frame.index = index;
+    });
+
+    res.status(200).json(lametricResponse);
 }
