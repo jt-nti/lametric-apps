51bf3ae76c166d23a21f92a64219721fafbc10e1
James Taylor <jt-nti@users.noreply.github.com>
2021-02-25 20:01:21 +0000

    Update Covid alert level
    https://www.bbc.co.uk/news/uk-56200738

--- api/lockdown.js
+++ api/lockdown.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-    const alertLevel = 5;
+    const alertLevel = 4;
     const start = new Date('March 24, 2020 00:00:00');
     const end = new Date('June 21, 2021 00:00:00');
     const now = new Date();
