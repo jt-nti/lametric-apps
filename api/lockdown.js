aec0899e7bb9b8e5e6d5a6e4d9e989b7da38cfb5
James Taylor <jt-nti@users.noreply.github.com>
2021-05-17 09:23:32 +0100

    Update Covid19 step number
    https://twitter.com/10DowningStreet/status/1394171437066375169

--- api/lockdown.js
+++ api/lockdown.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
     const alertLevel = 3;
-    const stepNumber = 2;
+    const stepNumber = 3;
     const start = new Date('March 24, 2020 00:00:00');
     const end = new Date('June 21, 2021 00:00:00');
     const now = new Date();
